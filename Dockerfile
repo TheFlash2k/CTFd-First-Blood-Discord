317a453cce06ee61f7b26a3692a19e5b034b2028
TheFlash2k <alitaqi2000@gmail.com>
2023-12-02 15:45:21 +0500

    Updated with newer content

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,7 @@
 FROM python:3.9
 
 WORKDIR /app
-
 COPY requirements.txt ./
-
 RUN pip install -r requirements.txt
-
 COPY ctfd-first-blood-discord/ ./
-
 CMD ["python", "-u", "main.py"]
--- ctfd-first-blood-discord/announcer.py
+++ ctfd-first-blood-discord/announcer.py
@@ -16,7 +16,6 @@ class Announcer:
     rate_limit_sleep_time: int
 
     def __init__(self):
-        self.webhook_url = config.WEBHOOK_URL
         self.solve_string = config.SOLVE_ANNOUNCE_STRING
         self.first_blood_string = config.FIRST_BLOOD_ANNOUNCE_STRING
         self.webhook_data = json.loads(
@@ -31,14 +30,15 @@ class Announcer:
                  first_blood: bool = False):
 
         async with aiohttp.ClientSession() as session:
-            webhook = Webhook.from_url(url=self.webhook_url, session=session)
 
             if first_blood:
+                webhook = Webhook.from_url(url=config.FIRST_BLOOD_WEBHOOK_URL, session=session)
                 await webhook.send(
                     self.first_blood_string.format(user_name=user_name,
                                                 chal_name=chal_name,
                                                 emojis=emoji))
             else:
+                webhook = Webhook.from_url(url=config.SOLVE_WEBHOOK_URL, session=session)
                 await webhook.send(
                     self.solve_string.format(user_name=user_name,
                                             chal_name=chal_name,
